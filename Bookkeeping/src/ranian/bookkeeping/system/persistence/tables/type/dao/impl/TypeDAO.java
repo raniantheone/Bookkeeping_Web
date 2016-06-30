5e32865c6889b7c024386eae4af791e90aac0260
Ranian <raniantheone@gmail.com>
2016-06-30 23:07:32 +0800

    Roughly implemented user and trans type query

--- Bookkeeping/src/ranian/bookkeeping/system/persistence/tables/type/dao/impl/TypeDAO.java
+++ Bookkeeping/src/ranian/bookkeeping/system/persistence/tables/type/dao/impl/TypeDAO.java
@@ -1,8 +1,5 @@
 package ranian.bookkeeping.system.persistence.tables.type.dao.impl;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
--- Bookkeeping/src/ranian/bookkeeping/system/persistence/tables/user/dao/impl/UserDAO.java
+++ Bookkeeping/src/ranian/bookkeeping/system/persistence/tables/user/dao/impl/UserDAO.java
@@ -1,10 +1,15 @@
 package ranian.bookkeeping.system.persistence.tables.user.dao.impl;
 
+import ranian.bookkeeping.system.persistence.tables.BaseDAO;
 import ranian.bookkeeping.system.persistence.tables.user.dao.IUserDAO;
 import ranian.bookkeeping.system.persistence.tables.user.vo.UserVO;
 
-public class UserDAO implements IUserDAO {
-
+public class UserDAO extends BaseDAO implements IUserDAO {
+	
+	public UserDAO() {
+		super();
+	}
+	
 	@Override
 	public Boolean insertUser(UserVO user) {
 		// TODO Auto-generated method stub
@@ -25,8 +30,37 @@ public class UserDAO implements IUserDAO {
 
 	@Override
 	public UserVO getUserById(Integer userId) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String sqlCmd = "select USER_ID, USER_LOGIN_ACC, USER_LOGIN_PW, "
+				+ "USER_NAME, USER_MAIL "
+				+ "from USER_AUTHEN "
+				+ "where USER_ID = ?";
+		UserVO userVo = new UserVO();
+		
+		try{
+			
+			conn = connUtil.getMysqlConnection();
+			pstmt = conn.prepareStatement(sqlCmd);
+			pstmt.setInt(1, userId);
+			rs = pstmt.executeQuery();
+			while( rs.next() ) {
+				
+				
+				userVo.setUserId(rs.getInt("USER_ID"));
+				userVo.setUserLoginAcc(rs.getString("USER_LOGIN_ACC"));
+				userVo.setUserLoginPw(rs.getString("USER_LOGIN_PW"));
+				userVo.setUserName(rs.getString("USER_NAME"));
+				userVo.setUserMail(rs.getString("USER_MAIL"));
+				
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeResources(rs, pstmt, conn);
+		}
+		
+		return userVo;
 	}
 
 	@Override
